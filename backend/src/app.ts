51730a1b53f3ee2acd7a8b2af52293ab294ad6a0
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-10-20 11:38:54 -0300

    backend - setup server + initialize

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,24 @@
 
+// imports
+import express, { Express, Application } from "express";
+
+// import server setup
+import { Server } from "./server";
+
+
+// class
+class App {
+  
+   // initialize app
+   public initialize(): void {
+      // express instance
+      const app: Express = express();
+      const server: Server = new Server(app);
+
+      // start server
+      server.start();
+   };
+
+};
+const app: App = new App();
+app.initialize();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,35 @@
 
+// imports
+import { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+
+
+// utils
+const SERVER_PORT = 5111;
+
+
+// class - Server
+export class Server {
+   // express instance
+   private app: Application;
+
+   // constructor
+   constructor(app: Application){
+      this.app = app;
+   };
+
+   // start methods
+   public start(): void {
+      this.startServer(this.app);
+   };
+
+   // start server
+   private startServer(app: Application): void {
+      app.listen(SERVER_PORT, () => {
+         console.log('Server initialize at port: ', SERVER_PORT);
+      });
+   };
+
+};
