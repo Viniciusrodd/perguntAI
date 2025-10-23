b755d22f3af19ac3e951f0ccdda4cf97c16368fc
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-10-23 11:13:54 -0300

    backend - pdfkit + pdf config

---
+++ backend/src/config/pdf.config.ts
@@ -0,0 +1,14 @@
+
+
+export const pdfConfig = {
+   outputDir: 'src/temp/pdf',
+   meta: {
+      title: 'Relatório de avaliação',
+      author: 'perguntAI App',
+      subject: 'Resumo de desempenho do usuário'
+   },
+   styles: {
+      headerColor: "#003366",
+      accentColor: "#007ACC",
+   }
+};
