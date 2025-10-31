551cc2e2226d97e1d74799044b99fe39c09744a9
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-10-31 18:50:18 -0300

    frontend - question language context implementation

--- frontend/src/components/home/Language.tsx
+++ frontend/src/components/home/Language.tsx
@@ -7,9 +7,32 @@ import NextPageBtt from '../NextPageBtt';
 import ExitBtt from '../ExitBtt';
 import ProgressBar from '../ProgressBar';
 
+// import hooks
+import { useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+
+// import context
+import { OptionsContext } from '../../contexts/QuestionsOptions/OptionsContext';
+
 
 // language
 const Language = () => {
+   // variables
+   const navigate = useNavigate();
+   const { numQuestions, difficulty, questionType, text, language, setLanguage } = useContext(OptionsContext);
+
+   // functions
+   const nextBtt = () =>{
+      console.log(text)
+      console.log(numQuestions)
+      console.log(difficulty)
+      console.log(questionType)
+      console.log(language);
+      navigate('/home/questions');
+   };
+
+   // jsx
+
    return (
       <div className='questions_default_container'>
          { /* progress bar 100% */ }
@@ -20,15 +43,21 @@ const Language = () => {
          </h1>
 
          <div className={ styles.questions_container }>
-            <select title='questions' name="questions" className={ styles.questions_select }>
-               <option value="" selected>Selecione uma opção</option>
+            <select 
+               title='questions' 
+               name="questions" 
+               className={ styles.questions_select }
+               value={ language }
+               onChange={ (e: React.ChangeEvent<HTMLSelectElement>) => setLanguage(e.target.value) }
+            >
+               <option value="">Selecione uma opção</option>
                <option value="portuguese">Português</option>
                <option value="english">Inglês</option>
             </select>
          </div>
 
          { /* next page button */ }
-         <NextPageBtt destiny='questions' />
+         <NextPageBtt destiny={ nextBtt } />
 
          { /* exit button */ }
          <ExitBtt destiny='questionsType' />
