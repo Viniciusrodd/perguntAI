13e39c644aa62fa58d53abdaf15e107987c2877f
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-10-31 18:40:53 -0300

    frontend - question difficulty context implementation

--- frontend/src/components/home/Difficulty.tsx
+++ frontend/src/components/home/Difficulty.tsx
@@ -7,9 +7,27 @@ import NextPageBtt from '../NextPageBtt';
 import ExitBtt from '../ExitBtt';
 import ProgressBar from '../ProgressBar';
 
+// import hooks
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+
+// import context
+import { OptionsContext } from '../../contexts/QuestionsOptions/OptionsContext';
+
 
 // difficulty
 const Difficulty = () => {
+   // variables
+   const navigate = useNavigate();
+   const { difficulty, setDifficulty } = useContext(OptionsContext);
+
+   // functions
+   const nextBtt = () =>{
+      navigate('/home/questionsType');
+   };
+
+   // jsx
+
    return (
       <div className='questions_default_container'>
          { /* progress bar 50% */ }
@@ -20,8 +38,14 @@ const Difficulty = () => {
          </h1>
 
          <div className={ styles.questions_container }>
-            <select title='questions' name="questions" className={ styles.questions_select }>
-               <option value="" selected>Selecione uma opção</option>
+            <select 
+               title='questions' 
+               name="questions" 
+               className={ styles.questions_select }
+               value={ difficulty }
+               onChange={ (e: React.ChangeEvent<HTMLSelectElement>) => setDifficulty(e.target.value) }
+            >
+               <option value="">Selecione uma opção</option>
                <option value="basic">Básica</option>
                <option value="intermediate">Intermediária</option>
                <option value="advanced">Avançada</option>
@@ -29,7 +53,7 @@ const Difficulty = () => {
          </div>
 
          { /* next page button */ }
-         <NextPageBtt destiny='questionsType' />
+         <NextPageBtt destiny={ nextBtt } />
 
          { /* exit button */ }
          <ExitBtt destiny='questionsNumber' />
