88c34e36d2a3505cbd9a85af0007675916ac388b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-11-06 09:09:36 -0300

    frontend - welcome page modal implementation

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -7,24 +7,90 @@ import welcome_robot_img from '../../public/images/welcome/welcome_robot.png'
 
 // import hooks
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+// import interfaces
+import type { iModalConfig } from '../../../shared/interfaces/modal.interface';
+
+// import components
+import Modal from '../components/Modal';
 
 
 // welcome
 const Welcome = () => {
-
-   // variables
+   
+   //// variables
    const navigate = useNavigate();
+   const [ modal_display, setModal_display ] = useState<boolean>(false);
+   const [ modal_title, setModal_title ] = useState<string>('');
+   const [ modal_msg, setModal_msg ] = useState<string>('');
+   const [ modal_btt, setmodal_btt ] = useState<boolean | string>(false);
+   const [ modal_btt_2, setModal_btt_2 ] = useState<boolean | string>(false);
+   const [ modal_event, setModal_event ] = useState<string>('');
 
-   // functions
-   const beginning_btt = () =>{
+
+   //// functions
+
+
+   // modal config
+   const modal_config = ({ title, msg, btt1, btt2, display }: iModalConfig) => {
+      setModal_title(title ?? '');
+      setModal_msg(msg ?? '');
+      setmodal_btt(btt1 ?? false);
+      setModal_btt_2(btt2 ?? false);
+      setModal_display(display ?? false);
+   };   
+
+   // close modal
+   const closeModal = () =>{
+      modal_config({
+         title: '', msg: '', btt1: false, 
+         btt2: false, display: false
+      });
+   };
+
+   // modal event handler
+   const modal_event_handler = () =>{
+      if(modal_event === 'base_text'){
+         base_text();
+      }
+   };
+
+   // modal advice
+   const modal_advice = () =>{
+      modal_config({
+         title: 'Como funciona o perguntAI:', 
+         msg: `Você nos da um texto 📃 \n e com base nele, \n geramos quantas questões desejar 🦾 \n 
+         🤖 simples assim 🤖`, 
+         btt1: 'Bora!', btt2: false, display: true
+      });
+
+      // call btt1 event
+      setModal_event('base_text')
+   }
+
+   // base text - navigate
+   const base_text = () =>{
       navigate('/home/baseText');
    };
    
 
-   // jsx
+   //// jsx
+
 
    return (
       <div className={ styles.welcome_container }>
+         { /* modal */ }
+         <Modal 
+            title={ modal_title }
+            msg={ modal_msg }
+            btt1={ modal_btt }
+            btt2={ modal_btt_2 }
+            display={ modal_display }
+            onClose={ closeModal }
+            modalEvent={ modal_event_handler }
+         />
+
          <img 
             src={ welcome_robot_img } 
             alt="welcome_robot_image" 
@@ -35,7 +101,7 @@ const Welcome = () => {
             Olá, bem vindo/a ao PerguntAI
          </h1>
 
-         <button className={ styles.start_button } type='button' onClick={ beginning_btt }>
+         <button className={ styles.start_button } type='button' onClick={ modal_advice }>
             começar
          </button>
       </div>
--- shared/interfaces/modal.interface.ts
+++ shared/interfaces/modal.interface.ts
@@ -8,3 +8,11 @@ export interface iModal{
    onClose: () => void; 
    modalEvent?: (event: any) => void;
 };
+
+export interface iModalConfig{
+   title: string;
+   msg: string;
+   btt1: boolean | string;
+   btt2: boolean | string;
+   display: boolean;
+};
