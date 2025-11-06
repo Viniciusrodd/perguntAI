1794903868f97bd79f482456e49f781b130121b6
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-11-06 10:05:07 -0300

    frontend - questions type component modal implementation

--- frontend/src/components/home/QuestionsType.tsx
+++ frontend/src/components/home/QuestionsType.tsx
@@ -6,13 +6,17 @@ import styles from '../../styles/home/Questions.module.css';
 import NextPageBtt from '../NextPageBtt';
 import ExitBtt from '../ExitBtt';
 import ProgressBar from '../ProgressBar';
+import Modal from '../Modal';
 
 // import hooks
 import { useNavigate } from 'react-router-dom';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 // import context
 import { OptionsContext } from '../../contexts/QuestionsOptions/Options.context';
+
+// import interfaces
+import type { iModalConfig } from '../../../../shared/interfaces/modal.interface';
 
 // types
 type QuestionType = 'open' | 'multipleChoice' | 'mix';
@@ -20,19 +24,71 @@ type QuestionType = 'open' | 'multipleChoice' | 'mix';
 
 // questions type
 const QuestionsType = () => {
-   // variables
+   
+   //// variables
    const navigate = useNavigate();
    const { questionType, setQuestionType } = useContext(OptionsContext);
+   const [ modal_display, setModal_display ] = useState<boolean>(false);
+   const [ modal_title, setModal_title ] = useState<string>('');
+   const [ modal_msg, setModal_msg ] = useState<string>('');
+   const [ modal_btt, setmodal_btt ] = useState<boolean | string>(false);
+   const [ modal_btt_2, setModal_btt_2 ] = useState<boolean | string>(false);
 
-   // functions
+
+   //// functions
+
+   
+   // modal config
+   const modal_config = ({ title, msg, btt1, btt2, display }: iModalConfig) => {
+      setModal_title(title ?? '');
+      setModal_msg(msg ?? '');
+      setmodal_btt(btt1 ?? false);
+      setModal_btt_2(btt2 ?? false);
+      setModal_display(display ?? false);
+   };   
+
+   // close modal
+   const closeModal = () =>{
+      modal_config({
+         title: '', msg: '', btt1: false, 
+         btt2: false, display: false
+      });
+   };
+
+   // question type check
+   const question_check = () =>{
+      if(!questionType){
+         modal_config({
+            title: 'Só um segundo ❗️', 
+            msg: `Insira um tipo de questão que deseja para prosseguir 🔍`, 
+            btt1: false, btt2: 'Voltar', display: true
+         });
+      }else{
+         nextBtt();
+      }
+   }    
+
+   // language - navigate
    const nextBtt = () =>{
       navigate('/home/language');
    };
 
-   // jsx
+   
+   //// jsx
+
 
    return (
       <div className='questions_default_container'>
+         { /* modal */ }
+         <Modal 
+            title={ modal_title }
+            msg={ modal_msg }
+            btt1={ modal_btt }
+            btt2={ modal_btt_2 }
+            display={ modal_display }
+            onClose={ closeModal }
+         />          
+     
          { /* progress bar 75% */ }
          <ProgressBar />
 
@@ -59,7 +115,7 @@ const QuestionsType = () => {
          </div>
 
          { /* next page button */ }
-         <NextPageBtt destiny={ nextBtt } />
+         <NextPageBtt destiny={ question_check } />
 
          { /* exit button */ }
          <ExitBtt destiny='difficulty' />
