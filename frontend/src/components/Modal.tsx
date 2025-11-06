96a1f83508a84a664b8599bcdb6554e1d60d19d7
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-11-06 08:24:56 -0300

    frontend - modal structure

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,16 +1,43 @@
 
 // import css
-import styles from '../styles/components/Modal.modules.css';
+import styles from '../styles/components/Modal.module.css';
 
 // import interfaces
-import { iModal } from "../../../shared/interfaces/modal.interface";
+import type { iModal } from "../../../shared/interfaces/modal.interface";
+
+// import hooks
+import type React from 'react';
 
 
 // modal
-const Modal = () => {
+const Modal: React.FC<iModal> = ({ title, msg, btt1, btt2, display, modalEvent, onClose }) => {
    return (
-      <div>
-      
+      <div className={ display ? styles.modal : styles.hidden }>
+         <div className={ styles.modal_content }>
+            <span onClick={ onClose } className={ `${styles.close_button} ${styles.span_btt}` }>
+               &times;
+            </span>
+            <h2 className={styles.h2_modal }>
+               { title }
+            </h2>
+            <p className={ styles.p_modal }>
+               { msg }                
+            </p>
+            {
+               btt1 && (
+                  <button onClick={ modalEvent } type='button' className={ styles.modal_button }>
+                     { btt1 }
+                  </button>
+               )
+            }
+            {
+               btt2 && (
+                  <button onClick={ onClose } type='button' className={ styles.close_modal_button }>
+                     { btt2 }
+                  </button>
+               )
+            }
+         </div>
       </div>
    );
 };
